31d7ae59cff1dadd3cf8bf5c7fca7ff74ca2d253
Otabek <atabekabdusharipov@gmail.com>
2024-10-19 00:40:04 +0500

    not commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let input = document.querySelector('.input')
+let btn = document.querySelector('.btn')
+let text = document.querySelector('.text')
+
+
+btn.addEventListener('click', () => {
+    let inputValue = input.value;
+    if (inputValue.trim()) {
+        const li = document.createElement('li')
+        li.textContent = inputValue
+
+
+        const delBtn = document.createElement('button')
+        delBtn.textContent = 'del'
+        delBtn.classList.add('delBtn')
+
+        delBtn.addEventListener('click', () =>{
+            text.removeChild(li)
+        })
+
+
+        li.appendChild(delBtn)
+        text.prepend(li)
+        li.addEventListener('click', () =>{
+            li.style.textDecogitration = 'line-through'
+        })
+    }
+    input.value = ''
+})
